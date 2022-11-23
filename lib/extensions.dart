3a1531087aa5246e14a29cad82b86ca853b5c6e8
arabasta <arabasta@webspark.com>
2022-11-23 13:35:23 +0200

    Fixed extensions

--- lib/extensions.dart
+++ lib/extensions.dart
@@ -30,11 +30,11 @@ extension StringExt on String {
   String get languageNameByCode {
     switch (this) {
       case 'de':
-        return 'German';
+        return 'Deutsch';
       case 'en':
         return 'English';
       case 'es':
-        return 'Spanish';
+        return 'Español';
       case 'pt':
         return 'Portuguese';
       default:
@@ -137,10 +137,12 @@ extension IterableExtention<E> on Iterable<E> {
 
 extension DateTimeExt on DateTime {
   String get chatListTime {
-    DateTime now = DateTime.now().toUtc();
-    int timeDifference = DateTime(year, month, day)
-        .difference(DateTime(now.year, now.month, now.day))
-        .inDays;
+    DateTime now = DateTime.now();
+    DateTime localTime = toLocal();
+    int timeDifference =
+        DateTime(localTime.year, localTime.month, localTime.day)
+            .difference(DateTime(now.year, now.month, now.day))
+            .inDays;
     if (timeDifference == 0) {
       return DateFormat(dateFormat).format(this).parseDateTimePattern7;
     }
