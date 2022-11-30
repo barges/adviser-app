3f06a5e6216a7f8db337250437f1f4c2a1360e2e
arabasta <arabasta@webspark.com>
2022-11-30 09:47:34 +0200

    Localized messages

--- lib/data/models/enums/fortunica_user_status.dart
+++ lib/data/models/enums/fortunica_user_status.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:shared_advisor_interface/generated/l10n.dart';
 import 'package:shared_advisor_interface/presentation/themes/app_colors.dart';
