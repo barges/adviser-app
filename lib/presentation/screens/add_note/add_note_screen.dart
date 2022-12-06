491881a1a4bfaeac598f24f78d1ebe7580c68f3e
arabasta <arabasta@webspark.com>
2022-12-06 11:17:11 +0200

    FOR-2257 - Refactored Add note screen

--- lib/presentation/screens/add_note/add_note_screen.dart
+++ lib/presentation/screens/add_note/add_note_screen.dart
@@ -1,17 +1,16 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_advisor_interface/extensions.dart';
 import 'package:shared_advisor_interface/generated/assets/assets.gen.dart';
 import 'package:shared_advisor_interface/generated/l10n.dart';
-import 'package:shared_advisor_interface/main.dart';
 import 'package:shared_advisor_interface/main_cubit.dart';
 import 'package:shared_advisor_interface/presentation/common_widgets/appbar/wide_app_bar.dart';
 import 'package:shared_advisor_interface/presentation/common_widgets/buttons/app_icon_button.dart';
 import 'package:shared_advisor_interface/presentation/common_widgets/messages/app_error_widget.dart';
 import 'package:shared_advisor_interface/presentation/resources/app_constants.dart';
 import 'package:shared_advisor_interface/presentation/screens/add_note/add_note_cubit.dart';
+import 'package:shared_advisor_interface/presentation/screens/add_note/widgets/pictures_part_widget.dart';
+import 'package:shared_advisor_interface/presentation/screens/add_note/widgets/title_part_widget.dart';
 
 class AddNoteScreen extends StatelessWidget {
   const AddNoteScreen({
@@ -24,8 +23,6 @@ class AddNoteScreen extends StatelessWidget {
       create: (_) => AddNoteCubit(),
       child: Builder(builder: (context) {
         AddNoteCubit addNoteCubit = context.read<AddNoteCubit>();
-        List<String> imagesPaths =
-            context.select((AddNoteCubit cubit) => cubit.state.imagesPaths);
         bool isNoteNew =
             context.select((AddNoteCubit cubit) => cubit.state.isNoteNew);
         final bool isOnline = context.select(
@@ -64,55 +61,26 @@ class AddNoteScreen extends StatelessWidget {
                     children: [
                       isNoteNew
                           ? const SizedBox.shrink()
-                          : Text(
-                              addNoteCubit
-                                  .arguments.updatedAt!.parseDateTimePattern2,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .displaySmall
-                                  ?.copyWith(
-                                    fontWeight: FontWeight.w400,
-                                    color: Theme.of(context).shadowColor,
-                                  ),
+                          : Column(
+                              children: [
+                                Text(
+                                  addNoteCubit.arguments.updatedAt!
+                                      .parseDateTimePattern6,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .displaySmall
+                                      ?.copyWith(
+                                        fontWeight: FontWeight.w400,
+                                        color: Theme.of(context).shadowColor,
+                                      ),
+                                ),
+                                const SizedBox(
+                                  height: 12.0,
+                                )
+                              ],
                             ),
 
-                      // Padding(
-                      // padding: const EdgeInsets.only(top: 12.0),
-                      // child: Builder(builder: (context) {
-                      // bool hadTitle = context
-                      // .select((AddNoteCubit cubit) => cubit.state.hadTitle);
-                      // if (hadTitle) {
-                      // return TextField(
-                      // controller: addNoteCubit.titleController,
-                      // autofocus: true,
-                      // style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                      // fontWeight: FontWeight.w500,
-                      // ),
-                      // keyboardType: TextInputType.multiline,
-                      // maxLines: null,
-                      // scrollPadding: EdgeInsets.zero,
-                      // cursorColor: Theme.of(context).hoverColor,
-                      // decoration: const InputDecoration(
-                      // border: InputBorder.none,
-                      // focusedBorder: InputBorder.none,
-                      // enabledBorder: InputBorder.none,
-                      // errorBorder: InputBorder.none,
-                      // disabledBorder: InputBorder.none,
-                      // contentPadding: EdgeInsets.zero,
-                      // ),
-                      // );
-                      // } else {
-                      // return GestureDetector(
-                      // onTap: (() {
-                      // addNoteCubit.addTitle();
-                      // }),
-                      // child: Text(S.of(context).title,
-                      // style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                      // fontWeight: FontWeight.w500,
-                      // color: Theme.of(context).shadowColor)));
-                      // }
-                      // }),
-                      // ),
+                      //const TitlePartWidget(),
 
                       TextField(
                         controller: addNoteCubit.noteController,
@@ -134,40 +102,7 @@ class AddNoteScreen extends StatelessWidget {
                           contentPadding: EdgeInsets.zero,
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 6.0),
-                        child: Center(
-                          child: Builder(builder: (context) {
-                            logger.d(imagesPaths);
-                            return (imagesPaths.isNotEmpty)
-                                ? Column(
-                                    children: List.generate(
-                                        imagesPaths.length,
-                                        (index) => Builder(builder: (context) {
-                                              final String imagesPath =
-                                                  context.select((AddNoteCubit
-                                                          cubit) =>
-                                                      cubit.state
-                                                          .imagesPaths[index]);
-                                              return Padding(
-                                                padding:
-                                                    const EdgeInsets.symmetric(
-                                                        vertical: 6.0),
-                                                child: Image.file(
-                                                  File(imagesPath),
-                                                  height: MediaQuery.of(context)
-                                                          .size
-                                                          .height /
-                                                      2,
-                                                  fit: BoxFit.cover,
-                                                ),
-                                              );
-                                            })),
-                                  )
-                                : const SizedBox.shrink();
-                          }),
-                        ),
-                      )
+                      //const PicturesPartWidget()
                     ]),
               ),
             ],
