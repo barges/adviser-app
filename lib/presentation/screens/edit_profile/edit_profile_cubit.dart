3689e4767a92e9258ac5306616b9a0856ad5021f
arabasta <arabasta@webspark.com>
2022-12-22 10:31:58 +0200

    FOR-2455 - Fixed a character limit in the Status field on the Edit profile page

--- lib/presentation/screens/edit_profile/edit_profile_cubit.dart
+++ lib/presentation/screens/edit_profile/edit_profile_cubit.dart
@@ -137,7 +137,9 @@ class EditProfileCubit extends Cubit<EditProfileState> {
         final ValidationErrorType statusErrorMessage =
             statusTextController.text.trim().isEmpty
                 ? ValidationErrorType.requiredField
-                : ValidationErrorType.empty;
+                : statusTextController.text.length > 300
+                    ? ValidationErrorType.statusTextMayNotExceed300Characters
+                    : ValidationErrorType.empty;
         final ValidationErrorType profileErrorMessage =
             profileTextController.text.trim().isEmpty
                 ? ValidationErrorType.requiredField
@@ -164,8 +166,15 @@ class EditProfileCubit extends Cubit<EditProfileState> {
 
     for (var entry in textControllersMap.entries) {
       entry.value.firstOrNull?.addListener(() {
-        errorTextsMap[entry.key]?.first = ValidationErrorType.empty;
-        emit(state.copyWith(updateTextsFlag: !state.updateTextsFlag));
+        if (entry.value.firstOrNull?.text != null &&
+            entry.value.firstOrNull!.text.length > 300) {
+          errorTextsMap[entry.key]?.first =
+              ValidationErrorType.statusTextMayNotExceed300Characters;
+          emit(state.copyWith(updateTextsFlag: !state.updateTextsFlag));
+        } else {
+          errorTextsMap[entry.key]?.first = ValidationErrorType.empty;
+          emit(state.copyWith(updateTextsFlag: !state.updateTextsFlag));
+        }
       });
       entry.value.lastOrNull?.addListener(() {
         errorTextsMap[entry.key]?.last = ValidationErrorType.empty;
