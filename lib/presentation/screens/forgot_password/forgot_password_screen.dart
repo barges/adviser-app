75ff1b6a79fbf20f7ad7e44742b86d7b8a5dacfe
arabasta <arabasta@webspark.com>
2023-01-04 09:47:10 +0200

    FOR-2325 - Refactored Forgot password screen for better widget testing

--- lib/presentation/screens/forgot_password/forgot_password_screen.dart
+++ lib/presentation/screens/forgot_password/forgot_password_screen.dart
@@ -20,6 +20,7 @@ import 'package:shared_advisor_interface/presentation/screens/forgot_password/fo
 import 'package:shared_advisor_interface/presentation/screens/forgot_password/widgets/email_part_widget.dart';
 import 'package:shared_advisor_interface/presentation/screens/forgot_password/widgets/reset_password_input_part.dart';
 import 'package:shared_advisor_interface/presentation/screens/forgot_password/widgets/success_reset.dart';
+import 'package:shared_advisor_interface/presentation/services/dynamic_link_service.dart';
 import 'package:shared_advisor_interface/presentation/utils/utils.dart';
 
 class ForgotPasswordScreen extends StatelessWidget {
@@ -27,137 +28,131 @@ class ForgotPasswordScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final MainCubit mainCubit = context.read<MainCubit>();
     return BlocProvider(
-      create: (_) => ForgotPasswordCubit(getIt.get<AuthRepository>()),
-      child: Builder(
-        builder: (context) {
-          final ForgotPasswordCubit cubit = context.read<ForgotPasswordCubit>();
-          final bool isOnline = context.select(
-              (MainCubit cubit) => cubit.state.internetConnectionIsAvailable);
-          return Scaffold(
-            appBar: SimpleAppBar(
-              title: S.of(context).forgotPassword,
-            ),
-            body: SafeArea(
-              child: isOnline
-                  ? Builder(builder: (context) {
-                      final bool isResetSuccess = context.select(
-                          (ForgotPasswordCubit cubit) =>
-                              cubit.state.isResetSuccess);
-                      return isResetSuccess
-                          ? const SuccessResetWidget()
-                          : GestureDetector(
-                              onTap: () {
-                                FocusScope.of(context).unfocus();
-                                cubit.clearErrorMessage();
+      create: (_) => ForgotPasswordCubit(getIt.get<AuthRepository>(),
+          getIt.get<DynamicLinkService>(), mainCubit),
+      child: const ForgotPasswordWidget(),
+    );
+  }
+}
+
+class ForgotPasswordWidget extends StatelessWidget {
+  const ForgotPasswordWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ForgotPasswordCubit cubit = context.read<ForgotPasswordCubit>();
+    final bool isOnline = context
+        .select((MainCubit cubit) => cubit.state.internetConnectionIsAvailable);
+    return Scaffold(
+      appBar: SimpleAppBar(
+        title: S.of(context).forgotPassword,
+      ),
+      body: SafeArea(
+        child: isOnline
+            ? Builder(builder: (context) {
+                final bool isResetSuccess = context.select(
+                    (ForgotPasswordCubit cubit) => cubit.state.isResetSuccess);
+                return isResetSuccess
+                    ? const SuccessResetWidget()
+                    : GestureDetector(
+                        onTap: () {
+                          FocusScope.of(context).unfocus();
+                          cubit.clearErrorMessage();
+                        },
+                        child: Column(
+                          children: [
+                            Builder(
+                              builder: (BuildContext context) {
+                                final AppError appError = context.select(
+                                    (MainCubit cubit) => cubit.state.appError);
+                                return appError is! EmptyError
+                                    ? AppErrorWidget(
+                                        errorMessage:
+                                            appError.getMessage(context),
+                                        close: () {
+                                          cubit.clearErrorMessage();
+                                        },
+                                      )
+                                    : const SizedBox.shrink();
                               },
-                              child: Column(
-                                children: [
-                                  Builder(
-                                    builder: (BuildContext context) {
-                                      final AppError appError = context.select(
-                                          (MainCubit cubit) =>
-                                              cubit.state.appError);
-                                      return appError is! EmptyError
-                                          ? AppErrorWidget(
-                                              errorMessage:
-                                                  appError.getMessage(context),
-                                              close: () {
-                                                cubit.clearErrorMessage();
-                                              },
-                                            )
-                                          : const SizedBox.shrink();
-                                    },
-                                  ),
-                                  Expanded(
-                                    child: SingleChildScrollView(
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: AppConstants
-                                              .horizontalScreenPadding),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
+                            ),
+                            Expanded(
+                              child: SingleChildScrollView(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal:
+                                        AppConstants.horizontalScreenPadding),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Padding(
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 24.0),
+                                      child: _BrandLogo(),
+                                    ),
+                                    Builder(builder: (context) {
+                                      final String? resetToken = context.select(
+                                          (ForgotPasswordCubit cubit) =>
+                                              cubit.state.resetToken);
+                                      return Column(
                                         children: [
-                                          const Padding(
-                                            padding: EdgeInsets.symmetric(
-                                                vertical: 24.0),
-                                            child: _BrandLogo(),
-                                          ),
+                                          resetToken == null
+                                              ? const EmailPart()
+                                              : const ResetPasswordInputPart(),
                                           Builder(builder: (context) {
-                                            final String? resetToken = context
-                                                .select((ForgotPasswordCubit
-                                                        cubit) =>
-                                                    cubit.state.resetToken);
-                                            return Column(
-                                              children: [
-                                                resetToken == null
-                                                    ? const EmailPart()
-                                                    : const ResetPasswordInputPart(),
-                                                Builder(builder: (context) {
-                                                  final bool isActive =
-                                                      context.select(
-                                                    (ForgotPasswordCubit
-                                                            cubit) =>
-                                                        cubit.state
-                                                            .isButtonActive,
-                                                  );
-                                                  return AppElevatedButton(
-                                                    title: resetToken == null
-                                                        ? S
-                                                            .of(context)
-                                                            .resetPassword
-                                                        : S
-                                                            .of(context)
-                                                            .changePassword,
-                                                    onPressed: isActive
-                                                        ? () =>
-                                                            cubit.resetPassword(
-                                                                resetToken)
-                                                        : null,
-                                                  );
-                                                }),
-                                              ],
+                                            final bool isActive =
+                                                context.select(
+                                              (ForgotPasswordCubit cubit) =>
+                                                  cubit.state.isButtonActive,
+                                            );
+                                            return AppElevatedButton(
+                                              title: resetToken == null
+                                                  ? S.of(context).resetPassword
+                                                  : S
+                                                      .of(context)
+                                                      .changePassword,
+                                              onPressed: isActive
+                                                  ? () => cubit
+                                                      .resetPassword(resetToken)
+                                                  : null,
                                             );
                                           }),
-                                          Padding(
-                                            padding: const EdgeInsets.symmetric(
-                                              vertical: 24.0,
-                                            ),
-                                            child: Utils.isDarkMode(context)
-                                                ? Assets.images.logos
-                                                    .forgotPasswordLogoDark
-                                                    .image(
-                                                    height:
-                                                        AppConstants.logoSize,
-                                                    width:
-                                                        AppConstants.logoSize,
-                                                  )
-                                                : Assets.images.logos
-                                                    .forgotPasswordLogo
-                                                    .image(
-                                                    height:
-                                                        AppConstants.logoSize,
-                                                    width:
-                                                        AppConstants.logoSize,
-                                                  ),
-                                          ),
                                         ],
+                                      );
+                                    }),
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                        vertical: 24.0,
                                       ),
+                                      child: Utils.isDarkMode(context)
+                                          ? Assets.images.logos
+                                              .forgotPasswordLogoDark
+                                              .image(
+                                              height: AppConstants.logoSize,
+                                              width: AppConstants.logoSize,
+                                            )
+                                          : Assets
+                                              .images.logos.forgotPasswordLogo
+                                              .image(
+                                              height: AppConstants.logoSize,
+                                              width: AppConstants.logoSize,
+                                            ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            );
-                    })
-                  : Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        NoConnectionWidget(),
-                      ],
-                    ),
-            ),
-          );
-        },
+                            ),
+                          ],
+                        ),
+                      );
+              })
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  NoConnectionWidget(),
+                ],
+              ),
       ),
     );
   }
