23050514bb62c2eff4f364eaf444cabf62d10d12
Oleksandr Zazhytskyi <>
2023-01-20 14:37:47 +0200

    FOR-2595 Flutter: Only portrait arrangement

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart' hide Transition;
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
@@ -58,6 +59,10 @@ void main() async {
     ServicesModule(),
     ApiModule(),
     RepositoryModule(),
+  ]);
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
   ]);
   runApp(const MyApp());
 }
