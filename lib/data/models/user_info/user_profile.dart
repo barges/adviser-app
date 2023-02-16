ffae976a3cf7b0097ba2097779dd1b0ce10c536b
MaksymNiskov <maksymniskov@gmail.com>
2023-02-16 23:12:31 +0400

    Fix for FR market

--- lib/data/models/user_info/user_profile.dart
+++ lib/data/models/user_info/user_profile.dart
@@ -13,18 +13,13 @@ part 'user_profile.g.dart';
 class UserProfile with _$UserProfile {
   @JsonSerializable(includeIfNull: false)
   const factory UserProfile({
-    Map<MarketsType, double?>? rating,
-    List<MarketsType>? activeLanguages,
-    String? readerManager,
+    @JsonKey(fromJson: _ratingFromJson) Map<MarketsType, double?>? rating,
+    @JsonKey(fromJson: _activeLanguagesFromJson)
+        List<MarketsType>? activeLanguages,
     List<String>? profilePictures,
     List<String>? coverPictures,
     List<String>? galleryPictures,
-    List<String>? roles,
-    int? likeScore,
-    String? timezone,
-    int? loyaltyScore,
-    int? likesPercentage,
-    List<SessionsTypes>? rituals,
+    @JsonKey(fromJson: _ritualsFromJson) List<SessionsTypes>? rituals,
     bool? isTestAccount,
     LocalizedProperties? localizedProperties,
     String? profileName,
@@ -32,4 +27,41 @@ class UserProfile with _$UserProfile {
 
   factory UserProfile.fromJson(Map<String, dynamic> json) =>
       _$UserProfileFromJson(json);
+}
+
+List<MarketsType>? _activeLanguagesFromJson(json) {
+  final List<MarketsType> markets = [];
+
+  (json as List<dynamic>?)?.forEach((element) {
+    final String market = element as String;
+    if (MarketsType.actualMarkets.contains(market)) {
+      markets.add(MarketsType.typeFromString(market));
+    }
+  });
+  return markets;
+}
+
+Map<MarketsType, double?>? _ratingFromJson(json) {
+  final Map<MarketsType, double?>? ratingMap = {};
+
+  (json as Map<String, dynamic>?)?.forEach((key, value) {
+    if (MarketsType.actualMarkets.contains(key)) {
+      if (value is double) {
+        ratingMap?[MarketsType.typeFromString(key)] = value;
+      }
+    }
+  });
+  return ratingMap;
+}
+
+List<SessionsTypes>? _ritualsFromJson(json) {
+  final List<SessionsTypes> rituals = [];
+
+  (json as List<dynamic>?)?.forEach((element) {
+    final String ritual = element as String;
+    if (SessionsTypes.actualSessionsTypes.contains(ritual)) {
+      rituals.add(SessionsTypes.typeFromString(ritual));
+    }
+  });
+  return rituals;
 }
