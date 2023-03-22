98d77ee8098c1cba3429e921a8364b399df157b0
MaksymNiskov <maksymniskov@gmail.com>
2023-03-22 20:44:25 +0400

    Fixed background push handler

--- lib/services/push_notification/push_notification_manager_impl.dart
+++ lib/services/push_notification/push_notification_manager_impl.dart
@@ -13,7 +13,6 @@ import 'package:fortunica/presentation/screens/chat/chat_screen.dart';
 import 'package:fortunica/presentation/screens/home/tabs_types.dart';
 import 'package:injectable/injectable.dart';
 import 'package:shared_advisor_interface/configuration.dart';
-import 'package:shared_advisor_interface/extensions.dart';
 import 'package:shared_advisor_interface/global.dart';
 import 'package:shared_advisor_interface/infrastructure/routing/app_router.dart';
 import 'package:shared_advisor_interface/infrastructure/routing/app_router.gr.dart';
@@ -191,6 +190,7 @@ class PushNotificationManagerImpl implements PushNotificationManager {
   }
 }
 
+@pragma('vm:entry-point')
 Future<void> _backgroundMessageHandler(RemoteMessage message) async {
   logger.d('***********************');
   logger.d(message.toMap());
