4ab596f4e7a51f2f883fe6870270b7c9588a1b5b
arabasta <arabasta@webspark.com>
2023-04-11 10:05:13 +0300

    ZS2-111 - Replaced InAppWebView with WebViewWidget

--- brands/zodiac/lib/presentation/screens/notification_details/notification_details_screen.dart
+++ brands/zodiac/lib/presentation/screens/notification_details/notification_details_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:shared_advisor_interface/global.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 import 'package:zodiac/domain/repositories/zodiac_user_repository.dart';
 import 'package:zodiac/infrastructure/di/inject_config.dart';
 import 'package:zodiac/presentation/common_widgets/appbar/transparrent_app_bar.dart';
@@ -35,65 +35,78 @@ class NotificationDetailsScreen extends StatelessWidget {
             fit: StackFit.expand,
             children: [
               Positioned.fill(
-                child: Builder(builder: (context) {
-                  final String? notificationContent = context.select(
-                      (NotificationDetailsCubit cubit) =>
-                          cubit.state.notificationContent);
-                  final bool loadStopped = context.select(
-                      (NotificationDetailsCubit cubit) =>
-                          cubit.state.loadStopped);
+                child: Builder(
+                  builder: (context) {
+                    final String? notificationContent = context.select(
+                        (NotificationDetailsCubit cubit) =>
+                            cubit.state.notificationContent);
+                    final bool loadStopped = context.select(
+                        (NotificationDetailsCubit cubit) =>
+                            cubit.state.loadStopped);
 
-                  return notificationContent != null
-                      ? IndexedStack(index: loadStopped ? 0 : 1, children: [
-                          InAppWebView(
-                            initialData: InAppWebViewInitialData(
-                              data: notificationContent,
-                            ),
-                            initialOptions: InAppWebViewGroupOptions(
-                              crossPlatform: InAppWebViewOptions(
-                                useShouldOverrideUrlLoading: true,
-                                supportZoom: false,
+                    return notificationContent != null
+                        ? IndexedStack(
+                            index: loadStopped ? 0 : 1,
+                            children: [
+                              WebViewWidget(
+                                controller: WebViewController()
+                                  ..setJavaScriptMode(
+                                      JavaScriptMode.unrestricted)
+                                  ..enableZoom(false)
+                                  ..setNavigationDelegate(
+                                    NavigationDelegate(
+                                      onPageFinished: (url) {
+                                        final NotificationDetailsCubit
+                                            notificationDetailsCubit =
+                                            context.read<
+                                                NotificationDetailsCubit>();
+
+                                        notificationDetailsCubit.stopLoading();
+                                      },
+                                      onNavigationRequest:
+                                          (NavigationRequest request) {
+                                        Uri? url = Uri.parse(request.url);
+                                        logger.d(url);
+
+                                        if (url.scheme.startsWith('http')) {
+                                          launchUrl(
+                                            url,
+                                            mode:
+                                                LaunchMode.externalApplication,
+                                          );
+                                        } else if (url.scheme == 'zodiac') {
+                                          final NotificationDetailsCubit
+                                              notificationDetailsCubit =
+                                              context.read<
+                                                  NotificationDetailsCubit>();
+                                          notificationDetailsCubit
+                                              .navigateFromNotification(
+                                                  context,
+                                                  url.host,
+                                                  url.queryParameters);
+                                        }
+
+                                        return NavigationDecision.prevent;
+                                      },
+                                    ),
+                                  )
+                                  ..loadHtmlString(notificationContent),
                               ),
-                            ),
-                            onLoadStop: (_, __) {
-                              final NotificationDetailsCubit
-                                  notificationDetailsCubit =
-                                  context.read<NotificationDetailsCubit>();
-
-                              notificationDetailsCubit.stopLoading();
-                            },
-                            shouldOverrideUrlLoading:
-                                (controller, navigationAction) async {
-                              Uri? url = navigationAction.request.url;
-                              logger.d(url);
-                              if (url != null) {
-                                if (url.scheme.startsWith('http')) {
-                                  launchUrl(
-                                    url,
-                                    mode: LaunchMode.externalApplication,
-                                  );
-                                } else if (url.scheme == 'zodiac') {
-                                  final NotificationDetailsCubit
-                                      notificationDetailsCubit =
-                                      context.read<NotificationDetailsCubit>();
-                                  notificationDetailsCubit
-                                      .navigateFromNotification(context,
-                                          url.host, url.queryParameters);
-                                }
-                              }
-
-                              return NavigationActionPolicy.CANCEL;
-                            },
-                          ),
-                          Center(
-                            child: CircularProgressIndicator(),
+                              const Center(
+                                child: CircularProgressIndicator(),
+                              ),
+                            ],
                           )
-                        ])
-                      : const SizedBox.shrink();
-                }),
+                        : const SizedBox.shrink();
+                  },
+                ),
               ),
               const Positioned(
-                  top: 0.0, right: 0.0, left: 0.0, child: TransparentAppBar()),
+                top: 0.0,
+                right: 0.0,
+                left: 0.0,
+                child: TransparentAppBar(),
+              ),
             ],
           ),
         ),
