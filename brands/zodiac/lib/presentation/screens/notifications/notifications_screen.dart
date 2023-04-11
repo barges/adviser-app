a639c686f7e71ae24dab7ca00998f3a8a06f68c9
arabasta <arabasta@webspark.com>
2023-04-11 12:01:35 +0300

    ZS2-66 - Added no internet connection state on Notifications screen

--- brands/zodiac/lib/presentation/screens/notifications/notifications_screen.dart
+++ brands/zodiac/lib/presentation/screens/notifications/notifications_screen.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shared_advisor_interface/app_constants.dart';
+import 'package:shared_advisor_interface/main_cubit.dart';
 import 'package:zodiac/data/models/notification/notification_item.dart';
 import 'package:zodiac/domain/repositories/zodiac_user_repository.dart';
 import 'package:zodiac/generated/l10n.dart';
 import 'package:zodiac/infrastructure/di/inject_config.dart';
 import 'package:zodiac/presentation/common_widgets/appbar/scrollable_appbar/scrollable_appbar.dart';
 import 'package:zodiac/presentation/common_widgets/empty_list_widget.dart';
+import 'package:zodiac/presentation/common_widgets/no_connection_widget.dart';
 import 'package:zodiac/presentation/screens/notifications/notifications_cubit.dart';
 import 'package:zodiac/presentation/screens/notifications/widgets/notifications_list_tile_widget.dart';
 import 'package:zodiac/zodiac_main_cubit.dart';
@@ -28,6 +30,9 @@ class NotificationsScreen extends StatelessWidget {
             context.read<NotificationsCubit>();
         final List<NotificationItem>? notifications = context
             .select((NotificationsCubit cubit) => cubit.state.notifications);
+        final bool internetConnectionIsAvailable = context.select(
+            (MainCubit cubit) => cubit.state.internetConnectionIsAvailable);
+
         return Scaffold(
           body: SafeArea(
             top: false,
@@ -50,39 +55,50 @@ class NotificationsScreen extends StatelessWidget {
                       height: 16.0,
                     ),
                   ),
-                  notifications != null
-                      ? notifications.isNotEmpty
-                          ? SliverList(
-                              delegate: SliverChildBuilderDelegate(
-                                (context, index) => NotificationsListTileWidget(
-                                    item: notifications[index]),
-                                childCount: notifications.length,
-                              ),
-                            )
-                          : SliverFillRemaining(
-                              hasScrollBody: false,
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                  horizontal:
-                                      AppConstants.horizontalScreenPadding,
-                                ),
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.max,
-                                  children: [
-                                    const Spacer(flex: 1),
-                                    EmptyListWidget(
-                                      title: SZodiac.of(context)
-                                          .noNotificationsYetZodiac,
-                                      label: SZodiac.of(context)
-                                          .yourNotificationsHistoryWillAppearHereZodiac,
+                  internetConnectionIsAvailable
+                      ? notifications != null
+                          ? notifications.isNotEmpty
+                              ? SliverList(
+                                  delegate: SliverChildBuilderDelegate(
+                                    (context, index) =>
+                                        NotificationsListTileWidget(
+                                            item: notifications[index]),
+                                    childCount: notifications.length,
+                                  ),
+                                )
+                              : SliverFillRemaining(
+                                  hasScrollBody: false,
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal:
+                                          AppConstants.horizontalScreenPadding,
                                     ),
-                                    const Spacer(flex: 2)
-                                  ],
-                                ),
-                              ),
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: [
+                                        const Spacer(flex: 1),
+                                        EmptyListWidget(
+                                          title: SZodiac.of(context)
+                                              .noNotificationsYetZodiac,
+                                          label: SZodiac.of(context)
+                                              .yourNotificationsHistoryWillAppearHereZodiac,
+                                        ),
+                                        const Spacer(flex: 2)
+                                      ],
+                                    ),
+                                  ),
+                                )
+                          : const SliverToBoxAdapter(
+                              child: SizedBox.shrink(),
                             )
-                      : const SliverToBoxAdapter(
-                          child: SizedBox.shrink(),
+                      : SliverFillRemaining(
+                          hasScrollBody: false,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              NoConnectionWidget(),
+                            ],
+                          ),
                         ),
                   const SliverToBoxAdapter(
                     child: SizedBox(
