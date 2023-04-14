79f34376cc16c8f4d7772e9063035529a0b08ae5
MaksymNiskov <maksymniskov@gmail.com>
2023-04-14 17:55:25 +0400

    FOR-2936 PROD version

--- lib/presentation/screens/home/tabs/account/account_cubit.dart
+++ lib/presentation/screens/home/tabs/account/account_cubit.dart
@@ -39,7 +39,7 @@ class AccountCubit extends Cubit<AccountState> {
 
   final PushNotificationManager _pushNotificationManager;
 
-  final Uri _url = Uri.parse(AppConstants.webToolUrlStage);
+  final Uri _url = Uri.parse(AppConstants.webToolUrlProd);
 
   late final VoidCallback disposeListen;
   late final StreamSubscription<bool> _appOnResumeSubscription;
