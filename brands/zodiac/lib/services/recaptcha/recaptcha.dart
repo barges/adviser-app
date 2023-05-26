8bed370bfad47953fd5055e5b712730ea46a7c3d
Oleksandr Zazhytskyi <>
2023-05-27 00:08:12 +0300

    ZS2-144 Implementation recaptcha service

---
+++ brands/zodiac/lib/services/recaptcha/recaptcha.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:recaptcha/recaptcha_enterprise.dart';
+import 'package:zodiac/data/models/enums/recaptcha_custom_action.dart';
+import 'package:zodiac/services/recaptcha/recaptcha_error.dart';
+
+class Recaptcha {
+  Recaptcha._();
+
+  static Completer<bool> _completer = Completer<bool>()..complete(false);
+
+  static Future<void> init(String siteKey) async {
+    if (!await isInitialized()) {
+      _completer = Completer<bool>();
+      try {
+        final initialize = await RecaptchaEnterprise.initialize(siteKey);
+        print('!!!! initialize:$initialize');
+        _completer.complete(true);
+      } on PlatformException catch (e) {
+        _completer.complete(false);
+        throw RecaptchaError(
+          code: e.code,
+          message: e.message,
+          details: e.details,
+        );
+      }
+    }
+  }
+
+  static Future<String> execute(
+      RecaptchaCustomAction recaptchaCustomAction) async {
+    return await RecaptchaEnterprise.execute(recaptchaCustomAction.toString());
+  }
+
+  static Future<bool> isInitialized() {
+    return _completer.future;
+  }
+}
