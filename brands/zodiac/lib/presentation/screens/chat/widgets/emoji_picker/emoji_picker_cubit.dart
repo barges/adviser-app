1846206b7abacd4b6662bbb1a2932b002c6a3c70
arabasta <arabasta@webspark.com>
2023-06-20 09:52:22 +0300

    ZS2-435 - Added emojis categories switching

--- brands/zodiac/lib/presentation/screens/chat/widgets/emoji_picker/emoji_picker_cubit.dart
+++ brands/zodiac/lib/presentation/screens/chat/widgets/emoji_picker/emoji_picker_cubit.dart
@@ -1,10 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zodiac/presentation/screens/chat/widgets/emoji_picker/emoji_picker_state.dart';
 
 class EmojiPickerCubit extends Cubit<EmojiPickerState> {
+  final PageController categoriesPageController = PageController();
+
   EmojiPickerCubit() : super(const EmojiPickerState());
 
-  void setCategoryIndex(int index) {
+  void setCategoryIndex(int index, {bool shouldJump = false}) {
     emit(state.copyWith(categoryIndex: index));
+    if (shouldJump) {
+      categoriesPageController.jumpToPage(index);
+    }
   }
 }
--- brands/zodiac/lib/presentation/screens/chat/widgets/emoji_picker/emoji_picker_widget.dart
+++ brands/zodiac/lib/presentation/screens/chat/widgets/emoji_picker/emoji_picker_widget.dart
@@ -3,11 +3,17 @@ import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:zodiac/presentation/screens/chat/widgets/emoji_picker/emoji_picker_cubit.dart';
 
-class EmojiPickerWidget extends StatelessWidget {
+class EmojiPickerWidget extends StatefulWidget {
   const EmojiPickerWidget({Key? key}) : super(key: key);
 
+  @override
+  State<EmojiPickerWidget> createState() => _EmojiPickerWidgetState();
+}
+
+class _EmojiPickerWidgetState extends State<EmojiPickerWidget> {
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
@@ -20,31 +26,55 @@ class EmojiPickerWidget extends StatelessWidget {
           final int categoryIndex = context
               .select((EmojiPickerCubit cubit) => cubit.state.categoryIndex);
           return Container(
-            height: 345,
+            height: 345.0,
             color: theme.canvasColor,
             padding:
                 const EdgeInsets.symmetric(vertical: 12.0, horizontal: 8.0),
             child: EmojiPicker(
               customWidget: (config, state) {
                 final List<CategoryEmoji> categories = state.categoryEmoji;
-                final List<Emoji> emojis = categories[categoryIndex].emoji;
+
                 return Column(
                   children: [
                     Expanded(
-                      child: GridView.builder(
-                        gridDelegate:
-                            const SliverGridDelegateWithMaxCrossAxisExtent(
-                                maxCrossAxisExtent: 40.0,
-                                mainAxisSpacing: 10.0,
-                                crossAxisSpacing: 10.0),
-                        itemBuilder: (context, index) => Center(
-                          child: Text(
-                            emojis[index].emoji,
-                            style: theme.textTheme.bodyMedium
-                                ?.copyWith(fontSize: 30.0),
-                          ),
+                        child: PageView.builder(
+                            controller:
+                                emojiPickerCubit.categoriesPageController,
+                            itemCount: categories.length,
+                            onPageChanged: emojiPickerCubit.setCategoryIndex,
+                            itemBuilder: (context, index) {
+                              final List<Emoji> emojis =
+                                  categories[index].emoji;
+
+                              return GridView.builder(
+                                gridDelegate:
+                                    const SliverGridDelegateWithMaxCrossAxisExtent(
+                                        maxCrossAxisExtent: 40.0,
+                                        mainAxisSpacing: 10.0,
+                                        crossAxisSpacing: 10.0),
+                                itemBuilder: (context, index) => Center(
+                                  child: Text(
+                                    emojis[index].emoji,
+                                    style: theme.textTheme.bodyMedium
+                                        ?.copyWith(fontSize: 30.0),
+                                  ),
+                                ),
+                                itemCount: emojis.length,
+                              );
+                            })),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 12.0),
+                      child: SmoothPageIndicator(
+                        controller: emojiPickerCubit.categoriesPageController,
+                        count: categories.length,
+                        effect: ScrollingDotsEffect(
+                          activeDotColor: theme.primaryColor,
+                          activeDotScale: 1.0,
+                          dotColor: theme.hintColor,
+                          maxVisibleDots: 5,
+                          dotHeight: 8.0,
+                          dotWidth: 8.0,
                         ),
-                        itemCount: emojis.length,
                       ),
                     ),
                     Container(
@@ -60,8 +90,8 @@ class EmojiPickerWidget extends StatelessWidget {
                           children: categories.mapIndexed((index, element) {
                             final bool isSelected = categoryIndex == index;
                             return GestureDetector(
-                              onTap: () =>
-                                  emojiPickerCubit.setCategoryIndex(index),
+                              onTap: () => emojiPickerCubit
+                                  .setCategoryIndex(index, shouldJump: true),
                               child: Container(
                                 height: 32.0,
                                 width: 32.0,
