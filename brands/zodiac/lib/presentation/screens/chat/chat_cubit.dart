651905d402c431cc25d2f99bd801b69c45955150
arabasta <arabasta@webspark.com>
2023-08-23 09:57:11 +0300

    ZS2-883 - Added reply to own message in current chat

--- brands/zodiac/lib/presentation/screens/chat/chat_cubit.dart
+++ brands/zodiac/lib/presentation/screens/chat/chat_cubit.dart
@@ -527,6 +527,8 @@ class ChatCubit extends BaseCubit<ChatState> {
       message: text,
       repliedMessage: repliedMessage,
       repliedMessageId: state.repliedMessage?.id,
+      supportsReply: true,
+      authorName: enterRoomData?.expertData?.name,
     );
     if (state.needShowDownButton) {
       animateToStartChat();
