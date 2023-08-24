d0cd45d1c75c68a7d80160db5fcd3ee21a6384f6
arabasta <arabasta@webspark.com>
2023-08-24 10:46:21 +0300

    ZS2-505 - Removed mocked duplicated services

--- brands/zodiac/lib/presentation/screens/duplicate_service/duplicate_service_cubit.dart
+++ brands/zodiac/lib/presentation/screens/duplicate_service/duplicate_service_cubit.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:zodiac/data/models/enums/service_type.dart';
-import 'package:zodiac/data/models/services/service_info_item.dart';
 import 'package:zodiac/data/models/services/service_item.dart';
-import 'package:zodiac/data/models/services/service_language_model.dart';
 import 'package:zodiac/data/network/requests/get_service_info_request.dart';
 import 'package:zodiac/data/network/requests/services_list_request.dart';
 import 'package:zodiac/data/network/responses/get_service_info_response.dart';
@@ -73,50 +70,3 @@ class DuplicateServiceCubit extends Cubit<DuplicateServiceState> {
     }
   }
 }
-
-List<ServiceInfoItem> servicesInfo = const [
-  ServiceInfoItem(
-    id: 1,
-    image:
-        'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
-    mainLocale: 'en',
-    type: ServiceType.offline,
-    translations: [
-      ServiceLanguageModel(
-        code: 'en',
-        title: 'Karma cleaning EN',
-        description: 'Some description EN',
-      ),
-      ServiceLanguageModel(
-        code: 'es',
-        title: 'Karma cleaning ES',
-        description: 'Some description ES',
-      )
-    ],
-    price: 19.99,
-    duration: 1800,
-    discount: 10,
-  ),
-  ServiceInfoItem(
-    id: 2,
-    image:
-        'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
-    mainLocale: 'es',
-    type: ServiceType.offline,
-    translations: [
-      ServiceLanguageModel(
-        code: 'en',
-        title: 'Tarrot Reading EN',
-        description: 'Some description EN',
-      ),
-      ServiceLanguageModel(
-        code: 'es',
-        title: 'Tarrot Reading ES',
-        description: 'Some description ES',
-      )
-    ],
-    price: 19.99,
-    duration: 1800,
-    discount: 10,
-  ),
-];
