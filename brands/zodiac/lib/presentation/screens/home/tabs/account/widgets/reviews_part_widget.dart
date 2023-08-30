16921684f02f1ada4f4d016572b5a09c020a4219
Oleksandr Zazhytskyi <>
2023-08-30 11:25:14 +0300

    ZS2-905 Fix incorrect display of the phone number on the Account page

--- brands/zodiac/lib/presentation/screens/home/tabs/account/widgets/reviews_part_widget.dart
+++ brands/zodiac/lib/presentation/screens/home/tabs/account/widgets/reviews_part_widget.dart
@@ -123,8 +123,7 @@ class ReviewsPartWidget extends StatelessWidget {
                 iconSVGPath: Assets.zodiac.vectors.call.path,
                 isDisable: !zodiacAccountCubit.isSiteKey(),
                 onTap: () => zodiacAccountCubit.goToPhoneNumber(context),
-                widget: SizedBox(
-                  width: 100.0,
+                widget: Flexible(
                   child: Text(
                     text,
                     overflow: TextOverflow.ellipsis,
