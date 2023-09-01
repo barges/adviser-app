52817a52785e7c07507769f5bd6aa0d3d626fb76
Oleksandr Zazhytskyi <>
2023-09-01 14:36:07 +0300

    ZS2-833 Set margin from red dot to audio recording time

--- brands/zodiac/lib/presentation/screens/chat/widgets/audio_recorder/chat_recording_widget.dart
+++ brands/zodiac/lib/presentation/screens/chat/widgets/audio_recorder/chat_recording_widget.dart
@@ -47,18 +47,16 @@ class ChatRecordingWidget extends StatelessWidget {
                 ),
                 child: Row(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(right: 2.5),
-                      child: Container(
-                        height: 11.0,
-                        width: 11.0,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: theme.errorColor,
-                          border: Border.all(
-                            width: 1.5,
-                            color: theme.canvasColor,
-                          ),
+                    Container(
+                      height: 11.0,
+                      width: 11.0,
+                      margin: const EdgeInsets.only(right: 2.5),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: theme.errorColor,
+                        border: Border.all(
+                          width: 1.5,
+                          color: theme.canvasColor,
                         ),
                       ),
                     ),
